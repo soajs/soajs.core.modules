dd1be9254c7628ab930db84ab9716d869749eaa1
antoinehage <me@antoinehage.com>
2017-05-04 10:22:49 -0400

    mapped service param into registry from config.js

--- soajs.core/registry/index.js
+++ soajs.core/registry/index.js
@@ -331,6 +331,11 @@ var build = {
                     };
                     newServiceObj.versions[param.serviceVersion] = {
                         "extKeyRequired": registry["services"][param.serviceName].extKeyRequired,
+                        "urac": registry["services"][param.serviceName].urac,
+                        "urac_Profile": registry["services"][param.serviceName].urac_Profile,
+                        "urac_ACL": registry["services"][param.serviceName].urac_ACL,
+                        "provision_ACL": registry["services"][param.serviceName].provision_ACL,
+                        "oauth": registry["services"][param.serviceName].oauth,
                         "apis": param.apiList
                     };
 
