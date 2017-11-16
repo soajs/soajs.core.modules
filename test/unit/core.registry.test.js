a645b258fe95ab156317af88109ffe9c3188dba3
Etienne <etienneadaher@gmail.com>
2017-11-16 13:52:34 +0200

    increasing coverage (+minor fixes)

--- test/unit/core.registry.test.js
+++ test/unit/core.registry.test.js
@@ -4,41 +4,63 @@ var assert = require('assert');
 var helper = require("../helper.js");
 var core = helper.requireModule('./index.js').core;
 var param = {
-    "serviceName": "unitTestRegistry",
-    "serviceGroup": "unit test",
-    "serviceVersion": 1,
-    "servicePort": 4999,
-    "extKeyRequired": false,
-    "requestTimeout": null,
-    "requestTimeoutRenewal": null,
-    "awareness": false,
-    "serviceIp": "127.0.0.1",
-    "swagger": false,
-    "apiList": {}
+	"serviceName": "unitTestRegistry",
+	"serviceGroup": "unit test",
+	"serviceVersion": 1,
+	"servicePort": 4999,
+	"extKeyRequired": false,
+	"requestTimeout": null,
+	"requestTimeoutRenewal": null,
+	"awareness": false,
+	"serviceIp": "127.0.0.1",
+	"swagger": false,
+	"apiList": {}
+};
+
+let metaData = {
+	"name": "core_provision",
+	"prefix": '',
+	"servers": [
+		{
+			"host": "127.0.0.1",
+			"port": 27017
+		}
+	],
+	"credentials": null,
+	"URLParam": {
+		"poolSize": 5,
+		"autoReconnect": true
+	}
 };
 
 describe("testing registry functionality", function () {
 	var myRegistry;
 	
-    it("Load registry", function (done) {
-        core.registry.load({
-            "serviceName": param.serviceName,
-            "serviceGroup": param.serviceGroup,
-            "serviceVersion": param.serviceVersion,
-            "designatedPort": param.servicePort,
-            "extKeyRequired": param.extKeyRequired,
-            "requestTimeout": param.requestTimeout,
-            "requestTimeoutRenewal": param.requestTimeoutRenewal,
-            "awareness": param.awareness,
-            "serviceIp": param.serviceIp,
-            "swagger": param.swagger,
-            "apiList": param.apiList
-        }, function (reg) {
-            assert.ok(reg);
-	        myRegistry = reg;
-            done();
-        });
-    });
+	after(function (done) {
+		// restore core provision, otherwise, the remaining integration test cases will fail
+		core.registry.init(metaData);
+		done();
+	});
+	
+	it("Load registry", function (done) {
+		core.registry.load({
+			"serviceName": param.serviceName,
+			"serviceGroup": param.serviceGroup,
+			"serviceVersion": param.serviceVersion,
+			"designatedPort": param.servicePort,
+			"extKeyRequired": param.extKeyRequired,
+			"requestTimeout": param.requestTimeout,
+			"requestTimeoutRenewal": param.requestTimeoutRenewal,
+			"awareness": param.awareness,
+			"serviceIp": param.serviceIp,
+			"swagger": param.swagger,
+			"apiList": param.apiList
+		}, function (reg) {
+			assert.ok(reg);
+			myRegistry = reg;
+			done();
+		});
+	});
 	
 	it("Register", function (done) {
 		var params = {
@@ -52,7 +74,7 @@ describe("testing registry functionality", function () {
 			requestTimeout: 30,
 			requestTimeoutRenewal: 5
 		};
-		core.registry.register(params, function(error, response){
+		core.registry.register(params, function (error, response) {
 			assert.ifError(error);
 			assert.ok(response);
 			done();
@@ -68,7 +90,7 @@ describe("testing registry functionality", function () {
 			'version': "1",
 			serviceHATask: "myServiceTask"
 		};
-		core.registry.registerHost(params, myRegistry, function(response){
+		core.registry.registerHost(params, myRegistry, function (response) {
 			assert.ok(response);
 			done();
 		});
@@ -86,7 +108,7 @@ describe("testing registry functionality", function () {
 			requestTimeout: 30,
 			requestTimeoutRenewal: 5
 		};
-		core.registry.register(params, function(error, response){
+		core.registry.register(params, function (error, response) {
 			assert.ifError(error);
 			assert.ok(response);
 			done();
@@ -102,7 +124,7 @@ describe("testing registry functionality", function () {
 			'version': "1",
 			serviceHATask: "myServiceTask"
 		};
-		core.registry.registerHost(params, myRegistry, function(response){
+		core.registry.registerHost(params, myRegistry, function (response) {
 			assert.ok(response);
 			done();
 		});
@@ -116,7 +138,7 @@ describe("testing registry functionality", function () {
 			'serviceHATask': "myServiceTask",
 			'serviceVersion': "1"
 		};
-		core.registry.autoRegisterService(params, function(response){
+		core.registry.autoRegisterService(params, function (response) {
 			assert.ok(response);
 			done();
 		});
@@ -142,40 +164,40 @@ describe("testing registry functionality", function () {
 		});
 	});
 	
-	it("Load registry custom", function (done) {
+	it("Load registry custom", function (done) { // return it in mocha -=-=-=-=-=
 		var custom = core.registry.getCustom("dev");
 		assert.ok(custom);
 		done();
 	});
 	
 	it("Profile", function (done) {
-		var custom = core.registry.profile(function(reg){
+		var custom = core.registry.profile(function (reg) {
 			assert.ok(reg);
 			done();
 		});
 	});
 	
-    it ("reLoad registry", function (done) {
-        core.registry.reload({
-	        "reload": true,
-            "serviceName": param.serviceName,
-            "serviceGroup": param.serviceGroup,
-            "serviceVersion": param.serviceVersion,
-            "designatedPort": param.servicePort,
-            "extKeyRequired": param.extKeyRequired,
-            "requestTimeout": param.requestTimeout,
-            "requestTimeoutRenewal": param.requestTimeoutRenewal,
-            "awareness": param.awareness,
-            "serviceIp": param.serviceIp,
-        }, function (err, reg) {
-            assert.ifError(err);
-            //console.log(reg);
-            assert.ok(reg);
-            done();
-        });
-    });
+	it("reLoad registry", function (done) {
+		core.registry.reload({
+			"reload": true,
+			"serviceName": param.serviceName,
+			"serviceGroup": param.serviceGroup,
+			"serviceVersion": param.serviceVersion,
+			"designatedPort": param.servicePort,
+			"extKeyRequired": param.extKeyRequired,
+			"requestTimeout": param.requestTimeout,
+			"requestTimeoutRenewal": param.requestTimeoutRenewal,
+			"awareness": param.awareness,
+			"serviceIp": param.serviceIp,
+		}, function (err, reg) {
+			assert.ifError(err);
+			//console.log(reg);
+			assert.ok(reg);
+			done();
+		});
+	});
 	
-	it ("reLoad registry", function (done) {
+	it("reLoad registry", function (done) {
 		core.registry.reload({
 			"reload": true,
 			"serviceName": "controller",
@@ -193,13 +215,13 @@ describe("testing registry functionality", function () {
 			done();
 		});
 	});
-    
-    it ("loadOtherEnvControllerHosts registry", function (done) {
-        core.registry.loadOtherEnvControllerHosts(function (err, reg) {
-            console.log(reg);
-            done();
-        });
-    });
+	
+	it("loadOtherEnvControllerHosts registry", function (done) {
+		core.registry.loadOtherEnvControllerHosts(function (err, reg) {
+			console.log(reg);
+			done();
+		});
+	});
 	
 	it("Load registry By Env", function (done) {
 		core.registry.loadByEnv({"envCode": "TEST"}, function (reg) {
@@ -207,4 +229,75 @@ describe("testing registry functionality", function () {
 			done();
 		});
 	});
+	
+	it("Register - unable to register service. missing params", function (done) {
+		let param = {};
+		core.registry.register(param, function (error, response) {
+			assert.equal(error.toString(), "Error: unable to register service. missing params");
+			done();
+		});
+	});
+	
+	it("Register - type !== service", function (done) {
+		let param = {
+			ip: "127.0.0.1",
+			name: "test",
+			port: 80
+		};
+		core.registry.register(param, function (error, response) {
+			assert.ok(response);
+			done();
+		});
+	});
+	
+	it("loadByEnv - error", function (done) {
+		let param = {
+			envCode: "dev"
+		};
+		
+		core.registry.loadByEnv(param, function (error, response) {
+			assert.ok(response);
+			done();
+		});
+	});
+	
+	it("loadOtherEnvControllerHosts - success", function (done) {
+		core.registry.loadOtherEnvControllerHosts(function (error, response) {
+			assert.ok(response);
+			done();
+		});
+	});
+	
+	it("registerHost - no servip", function (done) {
+		core.registry.registerHost({}, {}, function (error, response) {
+			assert.equal(error, false);
+			done();
+		});
+	});
+	
+	it("registerHost - no servip", function (done) {
+		let param = {
+			serviceIp: "127.0.0.1"
+		};
+		let registry = {
+			name: "test",
+			coreDB: {}
+		};
+		
+		core.registry.model = {
+			addUpdateServiceIP: function (provision, host, cb) {
+				return cb({message: "serviceX"}); // error
+			}
+		};
+		
+		try {
+			core.registry.registerHost(param, registry, function (error, response) {
+				
+			});
+		} catch (exception) {
+			assert.equal(exception.toString(), "Error: Unable to register new host for service:serviceX");
+			done();
+		}
+	});
+	
 });
