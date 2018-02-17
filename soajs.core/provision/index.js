88531c1d885bf2ff42cd3c40ed579fb7fe2d3e68
mikehajj <mike.hajj@gmail.com>
2018-02-17 21:44:42 +0200

    bug fix

--- soajs.core/provision/index.js
+++ soajs.core/provision/index.js
@@ -117,9 +117,9 @@ var provision = {
 				
 				//loop in tenant keys
 				oneApplication.keys.forEach(function (oneKey) {
-					
 					//saas mode detected, only look for the correct key based on soajs_project value in services config
-					if (process.env.SOAJS_SAAS && !tenant.locked && tenant.soajs_project && oneKey.config[env.toLowerCase()]) {
+					if (process.env.SOAJS_SAAS && tenant.soajs_project && oneKey.config[env.toLowerCase()]) {
+						
 						
 						let serviceConfig = (oneKey.config[env.toLowerCase()]) ? oneKey.config[env.toLowerCase()].SOAJS_SAAS : null;
 						if(!serviceConfig && oneKey.config[env.toLowerCase()].commonFields && oneKey.config[env.toLowerCase()].commonFields.SOAJS_SAAS){
@@ -128,6 +128,24 @@ var provision = {
 						
 						//if soajs_project is found in one of the applications configuration, then use ONLY that ext key
 						if(serviceConfig && serviceConfig[tenant.soajs_project]){
+							//loop in tenant ext keys
+							oneKey.extKeys.forEach(function (oneExtKey) {
+								//get the ext key for the request environment who also has dashboardAccess true
+								//note: only one extkey per env has dashboardAccess true, simply find it and break
+								if (oneExtKey.env && oneExtKey.env === env && oneExtKey.dashboardAccess && !oneExtKey.deprecated) {
+									extKey = oneExtKey.extKey;
+								}
+							});
+						}
+					}
+					else if (process.env.SOAJS_SAAS && oneKey.config[env.toLowerCase()]) {
+						let serviceConfig = (oneKey.config[env.toLowerCase()]) ? oneKey.config[env.toLowerCase()] : null;
+						if(!serviceConfig && oneKey.config[env.toLowerCase()].commonFields && oneKey.config[env.toLowerCase()].commonFields.SOAJS_SAAS){
+							serviceConfig = oneKey.config[env.toLowerCase()].commonFields;
+						}
+						
+						//if soajs_project is found in one of the applications configuration, then use ONLY that ext key
+						if(serviceConfig && !serviceConfig.SOAJS_SAAS){
 							//loop in tenant ext keys
 							oneKey.extKeys.forEach(function (oneExtKey) {
 								//get the ext key for the request environment who also has dashboardAccess true
