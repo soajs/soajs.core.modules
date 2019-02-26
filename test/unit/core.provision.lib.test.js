6c31043cee6c880427ba77188217d98c1b9ce4c7
antoinehage <me@antoinehage.com>
2019-02-26 12:30:24 -0500

    scope acl with ot without version tested

--- test/unit/core.provision.lib.test.js
+++ test/unit/core.provision.lib.test.js
@@ -5,19 +5,87 @@ var coreProvisionLib = helper.requireModule('./soajs.core/provision/lib');
 
 describe("core provision lib tests", function () {
 
-    let packACL = {
-        "dashboard": {
-            "oauth": [{
-                version: "1x2",
-                get: ["General"],
-                delete: ["General"]
-            }]
-        }
-    };
-    let scopeACL = {
-        "dashboard": {
-            "oauth": {
-                "1x2": {
+    it("success with version", function (done) {
+        let packACL = {
+            "dashboard": {
+                "oauth": [{
+                    version: "1x2",
+                    get: ["General"],
+                    delete: ["General"]
+                }]
+            }
+        };
+        let scopeACL = {
+            "dashboard": {
+                "oauth": {
+                    "1x2": {
+                        access: false,
+                        apisPermission: "restricted",
+                        get: {
+                            General: {
+                                apis: {
+                                    "/authorization": {}
+                                }
+                            }
+                        },
+                        post: {
+                            General: {
+                                apis: {
+                                    "/token": {}
+                                }
+                            }
+                        },
+                        delete: {
+                            General: {
+                                apis: {
+                                    "/accessToken/:token": {
+                                        access: false
+                                    },
+                                    "/refreshToken/:token": {
+                                        access: false
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        let what2expect = {
+            "dashboard": {
+                "oauth": {
+                    "1x2": {
+                        "access": false,
+                        "apisPermission": "restricted",
+                        "get": {"apis": {"/authorization": {}}},
+                        "delete": {
+                            "apis": {
+                                "/accessToken/:token": {"access": false},
+                                "/refreshToken/:token": {"access": false}
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        let acl = coreProvisionLib.getACLFromScope(scopeACL, packACL);
+        console.log(JSON.stringify(acl));
+        assert.deepStrictEqual(acl, what2expect, "Unable to create ACL from scope with version");
+        done();
+    });
+
+    it("success without version", function (done) {
+        let packACL = {
+            "dashboard": {
+                "oauth": [{
+                    get: ["General"],
+                    delete: ["General"]
+                }]
+            }
+        };
+        let scopeACL = {
+            "dashboard": {
+                "oauth": {
                     access: false,
                     apisPermission: "restricted",
                     get: {
@@ -46,32 +114,29 @@ describe("core provision lib tests", function () {
                             }
                         }
                     }
+
                 }
             }
-        }
-    };
-
-    it("success", function (done) {
+        };
         let what2expect = {
             "dashboard": {
                 "oauth": {
-                    "1x2": {
-                        "access": false,
-                        "apisPermission": "restricted",
-                        "get": {"apis": {"/authorization": {}}},
-                        "delete": {
-                            "apis": {
-                                "/accessToken/:token": {"access": false},
-                                "/refreshToken/:token": {"access": false}
-                            }
+                    "access": false,
+                    "apisPermission": "restricted",
+                    "get": {"apis": {"/authorization": {}}},
+                    "delete": {
+                        "apis": {
+                            "/accessToken/:token": {"access": false},
+                            "/refreshToken/:token": {"access": false}
                         }
                     }
+
                 }
             }
         };
         let acl = coreProvisionLib.getACLFromScope(scopeACL, packACL);
         console.log(JSON.stringify(acl));
-        assert.deepStrictEqual(acl, what2expect, "Unable to create ACL from scope");
+        assert.deepStrictEqual(acl, what2expect, "Unable to create ACL from scope without version");
         done();
     });
 
