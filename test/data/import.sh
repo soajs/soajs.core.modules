7e413d82731ab165d3037c6c2a5a133c7d9ac8d1
antoinehage <me@antoinehage.com>
2025-09-19 10:33:29 -0400

    fixed mongo import

--- test/data/import.sh
+++ test/data/import.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 pushd ./provision
+
 mongosh ./environment.js
 mongosh ./oauth.js
 mongosh ./product.js
 mongosh ./tenant.js
 mongosh ./resources.js
 mongosh ./customRegistry.js
+
 popd
